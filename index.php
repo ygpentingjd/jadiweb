7ac69c6b5d2b3a64940a1fd861c093e8162df80d
ygpentingjd <cc.banyumas@gmail.com>
2024-08-07 14:39:14 +0700

    new update

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 // Mendapatkan alamat IP server
 $server_ip = $_SERVER['SERVER_ADDR'];
-
+$kalimat .= "HELP AIM UNDEH DHE WATDHER PELIS HELP ME";
 // Menampilkan alamat IP server
 echo "Alamat IP server adalah: " . $server_ip;
+echo $kalimat;
 ?>
